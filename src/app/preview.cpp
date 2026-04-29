68405a1ae92556c8fbe2cd4b4c86fb8c3f7246df
Wayne Wu <wayne-wu@users.noreply.github.com>
2026-04-28 23:16:47 -0700

    Restructure source folders

--- src/app/preview.cpp
+++ src/app/preview.cpp
@@ -5,9 +5,9 @@
 #include <chrono>
 #include <iostream>
 
-#include "../imgui/imgui.h"
-#include "../imgui/imgui_impl_glfw.h"
-#include "../imgui/imgui_impl_opengl3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include "ImGuiFileDialog/ImGuiFileDialog.h"
 
 GLuint positionLocation = 0;
